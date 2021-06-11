807f844c556e57b647eeb6b09a4e3fc3648b58c0
“Pablo” <pggambero@gmail.com>
2021-06-11 19:03:21 +0200

    .

--- src/components/partials/exerciseItem.vue
+++ src/components/partials/exerciseItem.vue
@@ -1,5 +1,13 @@
 <template>
-  
+  <div class="container">
+    <div class="is-flex m-6">
+      <div class="is-flex is-flex-direction-column is-align-items-betwe mr-4 ml-4">
+        <div class>
+          
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
